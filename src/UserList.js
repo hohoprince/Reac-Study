b58f053ed6b91256ac128c9b1ac8120f4589b9ef
hohoprince <hoot0512@gmail.com>
2021-07-04 14:12:04 +0900

    배열에 항목 추가

--- src/UserList.js
+++ src/UserList.js
@@ -8,24 +8,7 @@ function User({ user }) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.veloper@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@gmail.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@gmail.com'
-        }
-    ];
+function UserList({ users }) {
 
     return (
         <div>
